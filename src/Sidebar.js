5e166a1bc460582bdef93d76c4a98e3c683b2ff8
Anne <annekhd98@gmail.com>
2022-05-06 18:47:00 -0300

    componentizacao ok

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import Sugestoes from "./Sugestoes"
+import Links from "./Links"
+
+export default function Sidebar() {
+  return (
+    <div class="sidebar">
+      <Sugestoes />
+      <Links />        
+    </div>
+  );
+}
