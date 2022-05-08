db333f6ff5ccf3c13b2040c1a99a70abf42b9aad
Anne <annekhd98@gmail.com>
2022-05-07 22:06:46 -0300

    Stories ok

--- src/Stories.js
+++ src/Stories.js
@@ -2,16 +2,51 @@ import StoryItem from "./StoryItem"
 
 export default function Stories() {
 
+const stories =[
+  {
+    url:"assets/img/9gag.svg",
+    user: "9gag"
+  },
+  {
+    url:"assets/img/meowed.svg",
+    user:"meowed"
+  },
+  {
+    url:"assets/img/barked.svg",
+    user:"barked"
+  },
+  {
+    url:"assets/img/nathanwpylestrangeplanet.svg",
+    user:"nathanwpylestrangeplanet"
+  },
+  {
+    url:"assets/img/wawawicomics.svg",
+    user:"wawawicomics"
+  },
+  {
+    url:"assets/img/respondeai.svg",
+    user:"respondeai"
+  },
+  {
+    url:"assets/img/filomoderna.svg",
+    user:"filomoderna"
+  },
+  {
+    url:"assets/img/memeriagourmet.svg",
+    user:"memeriagourmet"
+  }
+]
+
+const storiesJSX = stories.map(item => <StoryItem url={item.url} user={item.user} />)
+
   return (
     <div class ="stories">
-      <StoryItem url="assets/img/9gag.svg" user="9gag" />
-      <StoryItem url="assets/img/meowed.svg" user="meowed" />
-      <StoryItem url="assets/img/barked.svg"user="barked" />
-      <StoryItem url="assets/img/nathanwpylestrangeplanet.svg" user="nathanwpylestrangeplanet" />
-      <StoryItem url="assets/img/wawawicomics.svg" user="wawawicomics" />
-      <StoryItem url="assets/img/respondeai.svg" user="respondeai" />
-      <StoryItem url="assets/img/filomoderna.svg" user="filomoderna" />
-      <StoryItem url="assets/img/memeriagourmet.svg" user="memeriagourmet" />
+      {storiesJSX}
+      {storiesJSX}     
+      {storiesJSX}
+      {storiesJSX}
+      {storiesJSX}
+      {storiesJSX}
 
         <div class="setinha">
           <ion-icon name="chevron-forward-circle"></ion-icon>
--- src/StoryItem.js
+++ src/StoryItem.js
@@ -1,10 +1,11 @@
 export default function StoryItem(props){
-    return(
-      <div class="story">
-      <div class="imagem">
-        <img src={props.url} />
-      </div>
-      <div class="usuario">{props.user}</div>
+  return(
+    <div class="story">
+    <div class="imagem">
+      <img src={props.url} />
     </div>
-    )
-  }
+    <div class="usuario">{props.user}</div>
+  </div>
+  )
+}
+
