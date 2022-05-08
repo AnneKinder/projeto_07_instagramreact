3ff0f050b0d218b6540723921bd9fae1dc37d181
Anne <annekhd98@gmail.com>
2022-05-07 22:14:18 -0300

    posts ok

--- src/Posts.js
+++ src/Posts.js
@@ -2,10 +2,31 @@ import PostItem from "./PostItem"
 
 export default function Posts(){
   
+const posts =[
+  {
+    icon:"assets/img/meowed.svg",
+    user:"meowed",
+    imgpost:"assets/img/gato-telefone.svg",
+    imguser2:"assets/img/respondeai.svg",
+    user2:"respondeai",
+    qtpessoas:"101.523"
+  },
+  {
+    icon:"assets/img/barked.svg",
+    user:"meobarked",
+    imgpost:"assets/img/dog.svg",
+    imguser2:"assets/img/adorable_animals.svg",
+    user2:"adorable_animals",
+    qtpessoas:"99.159"
+  }
+]
+
+const postsJSX = posts.map(item=> <PostItem icon={item.icon} user={item.user} imgpost={item.imgpost} imguser2={item.imguser2} user2={item.user2} qtpessoas={item.qtpessoas} />)
+
   return (
     <div class="posts">
-      <PostItem icon="assets/img/meowed.svg" user="meowed" imgpost="assets/img/gato-telefone.svg" imguser2="assets/img/respondeai.svg" user2="respondeai" qtpessoas="101.523" />
-      <PostItem icon="assets/img/barked.svg" user="meobarked" imgpost="assets/img/dog.svg" imguser2="assets/img/adorable_animals.svg" user2="adorable_animals" qtpessoas="99.159" />
+      {postsJSX}
+      {postsJSX}
     </div>
   )
 }
